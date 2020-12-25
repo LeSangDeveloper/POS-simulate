6bb0853ac7e0938a03829786cb01faef54bdf65f
Le Sang <sang.le@mservice.com.vn>
2020-12-25 17:33:39 +0700

    edit server deloy localhost:300

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  server: {
+    port: 8000, // default: 3000
+    host: 'localhost' // default: localhost
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'POS-simulate',
